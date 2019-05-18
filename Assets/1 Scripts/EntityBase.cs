1d4c27a8e3d88ee2e15087e749554217fbc2686c
Robert-Thinkpad <rob5300@hotmail.com>
2019-05-18 13:20:41 +0100

    Added ground check and basic flying.

--- Assets/1 Scripts/EntityBase.cs
+++ Assets/1 Scripts/EntityBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,22 @@ using UnityEngine;
 public class EntityBase : MonoBehaviour
 {
     public float Health;
+    [NonSerialized]
+    public bool IsGrounded;
+    public Transform GroundCheckPoint;
 
     private Rigidbody2D rb;
+    private float groundDist;
 
     public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(GroundCheckPoint.position, -Vector2.up, 0.025f, LayerMask.GetMask("Ground"));
+        IsGrounded = (hit.collider != null);
     }
 
     public virtual void Damage(float damage)
--- Assets/1 Scripts/Player/PlayerMovement.cs
+++ Assets/1 Scripts/Player/PlayerMovement.cs
@@ -6,17 +6,41 @@ public class PlayerMovement : MonoBehaviour
     public Player player;
 
     public float MoveRate = 3;
+    public bool EnableFlying = true;
+    public bool EnableJump = true;
+
+    private bool shouldJump = false;
 
     public void Start()
     {
         if (!player) player = GetComponent<Player>();
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && player.IsGrounded)
+        {
+            shouldJump = true;
+        }
+    }
+
     public void FixedUpdate()
     {
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"), 0);
 
+        if (!player.IsGrounded)
+        {
+            input.y = Input.GetAxis("Vertical") * 0.8f;
+        } 
+
         player.Move(input, MoveRate);
+
+        if (shouldJump)
+        {
+            shouldJump = false;
+
+            player.Move(new Vector2(0, 5));
+        }
     }
 }
 

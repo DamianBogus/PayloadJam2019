ed66818b6e9eca532a5d21801c58153740b838e0
Robert-Thinkpad <rob5300@hotmail.com>
2019-05-18 11:58:19 +0100

    Added basic movement and entity movement.

--- Assets/1 Scripts/EntityBase.cs
+++ Assets/1 Scripts/EntityBase.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class EntityBase : MonoBehaviour
 {
     public float Health;
 
-    public void Damage()
-    {
+    private Rigidbody2D rb;
 
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Die()
+    public virtual void Damage(float damage)
     {
+        Health -= damage;
+        if(Health > 0)
+        {
+            Health = 0;
+            Die();
+        }
+    }
 
+    public virtual void Die()
+    {
+        //Do death stuff.
+    }
+
+    public virtual void Move(Vector2 direction, float multiplier = 3)
+    {
+        rb.AddForce(direction * multiplier, ForceMode2D.Impulse);
     }
 }
--- Assets/1 Scripts/Player/PlayerMovement.cs
+++ Assets/1 Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,22 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Player))]
 public class PlayerMovement : MonoBehaviour
 {
+    public Player player;
 
+    public float MoveRate = 3;
+
+    public void Start()
+    {
+        if (!player) player = GetComponent<Player>();
+    }
+
+    public void FixedUpdate()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        player.Move(input, MoveRate);
+    }
 }
 

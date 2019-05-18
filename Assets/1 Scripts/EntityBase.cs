b9f0bfb2e117b082abc75ab3079f0671c2d92605
Robert-Thinkpad <rob5300@hotmail.com>
2019-05-18 11:14:40 +0100

    Player and Enemy boilerplate

---
+++ Assets/1 Scripts/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityBase : MonoBehaviour
+{
+    public float Health;
+
+    public void Damage()
+    {
+
+    }
+
+    public void Die()
+    {
+
+    }
+}

9eb62ce499d13b7bb5921aca3d4cfaccb4401a06
damianbogus97 <damian.boguslawski99@gmail.com>
2019-05-18 14:53:26 +0100

    new enemy

---
+++ Assets/1 Scripts/Enemies/Fly.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : Enemy
+{
+    public override void AttackPlayer()
+    {
+        attackTimer += Time.deltaTime;
+
+        if (attackTimer > attackCooldown)
+        {
+            attackTimer = 0;
+           // rb.AddForce(Vector2.up * 100, ForceMode2D.Impulse);
+            target.Damage(DamageOutput);
+        }
+    }
+
+    public override void ChasePlayer()
+    {
+        direction.y = (transform.position.y <= target.transform.position.y) ? 1 : -1;
+        Move(direction, moveSpeed);
+    }
+
+
+}

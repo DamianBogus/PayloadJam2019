10f8ce50870302ece95ddcec92f7ed10b6c6885c
Robert-Thinkpad <rob5300@hotmail.com>
2019-05-18 13:26:58 +0100

    Added camera follow. Changed player prefab

---
+++ Assets/1 Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    [Range(0,1)]
+    public float LerpRate;
+
+    private void LateUpdate()
+    {
+        Vector3 targetpos = Target.position;
+        targetpos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, targetpos, LerpRate);
+    }
+}

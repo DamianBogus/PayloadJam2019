4d6b7eb3803a5689fc3089ce4431bd3463de1d69
Robert-Thinkpad <rob5300@hotmail.com>
2019-05-18 17:14:38 +0100

    Added buildings and tiles to map.

---
+++ Assets/1 Scripts/CameraParalax.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraParalax : MonoBehaviour
+{
+    public Transform Target;
+
+    void LateUpdate()
+    {
+        
+    }
+}

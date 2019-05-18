5897f47fb0824649176a82cfc9c8a8c921e5582c
damianbogus97 <damian.boguslawski99@gmail.com>
2019-05-18 11:52:23 +0100

    scripts for enemies

---
+++ Assets/1 Scripts/Enemies/Crawler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crawler : Enemy
+{
+
+    public override void AttackPlayer()
+    {
+
+    }
+
+    public override void ChasePlayer()
+    {
+
+    }
+
+}
---
+++ Assets/1 Scripts/Enemies/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : EntityBase
+{
+
+    public float DamageOutput = 2.0f;
+    public Transform target;
+    public bool inRange = false;
+    public float attackRange = 0.5f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        CheckPlayerInRange();
+
+        if (inRange) AttackPlayer();
+        else ChasePlayer();
+    }
+
+    private void CheckPlayerInRange()
+    {
+        inRange = (Vector2.Distance(target.position, transform.position) <= attackRange) ? true : false;
+    }
+
+    public virtual void AttackPlayer()
+    {
+
+    }
+
+    public virtual void ChasePlayer()
+    {
+
+    }
+
+}

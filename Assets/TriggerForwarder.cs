8ace690d27f23e7186a77ff98a0508ca663fddb7
ster1203 <ster1203@gmail.com>
2019-05-19 10:49:25 +0100

    Added collision detection and damage on enemies

---
+++ Assets/TriggerForwarder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class TriggerForwarder : MonoBehaviour
+{
+    public event Action<Collider2D> OnTriggerEvent;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnTriggerEvent?.Invoke(collision);
+    }
+}
